f4289ff81822160fe8ea70ccddb95c2bbe80be87
StickerQueen <s1743497@ed.ac.uk>
2019-11-17 21:43:00 +0000

    fixed mistake

--- src/control_helper.py
+++ src/control_helper.py
@@ -18,7 +18,7 @@ def forward_kinematics(angles):
 	c1,c2,c3,c4 = np.cos(theta1), np.cos(theta2), np.cos(theta3), np.cos(theta4)
 	s1,s2,s3,s4 = np.sin(theta1), np.sin(theta2), np.sin(theta3), np.sin(theta4)
 
-	x = (-c1*c2*(2*c3*c4+3*c3)) - s1*(2*s3*c4+3*s3) - 2*s1*c1*s4
+	x = (-c1*c2*(2*c3*c4+3*c3)) - s1*(2*s3*c4+3*s3) - 2*c1*s2*s4
 	y = s1*c2*(2*c3*c4+3*c3) + c1*(2*s3*c4+3*s3) - 2*s1*s2*s4
 	z = s2*(2*c3*c4+3*c3) + 2*c2*s4 + 2
 

d39113dd79ced24d55a868f659daae7df2cf66e5
StickerQueen <s1743497@ed.ac.uk>
2019-11-18 00:16:59 +0000

    added jacobian

--- src/control_helper.py
+++ src/control_helper.py
@@ -25,4 +25,30 @@ def forward_kinematics(angles):
 
 	return(np.array([x,y,z]))
 
-print(forward_kinematics([0,0,0,0]))
+def calculate_jacobian(angles):
+	theta1 = angles[0]
+	theta2 = angles[1]#+(np.pi/2)
+	theta3 = angles[2]
+	theta4 = angles[3]
+
+	c1,c2,c3,c4 = np.cos(theta1), np.cos(theta2), np.cos(theta3), np.cos(theta4)
+	s1,s2,s3,s4 = np.sin(theta1), np.sin(theta2), np.sin(theta3), np.sin(theta4)
+
+	jacobian = np.array([[
+	2*s1*c2*c3*c4-3*s1*c2*c3-2*c1*s3*c4+3*c1*s3+2*s1*s2*s4,
+	2*c1*s2*c3*c4-3*c1*s2*c3-2*c1*c3*s4,
+	2*c1*c2*s3*c4-3*c1*c2*s3-2*s1*c3*c4+3*s1*c3,
+	2*c1*c2*c3*s4+2*s1*s3*s4-2*c1*s2*c4],
+	[2*s1*c2*c3*c4+3*c1*c2*c3-2*s1*s3*c4-3*s1*s3-2*c1*s2*s4,
+	-2*s1*s2*c3*c4-3*s1*s2*c3-2*s1*c2*s4,
+	-2*s1*c2*s3*c4-3*s1*c2*s3+2*c1*c3*c4+3*c1*c3,
+	-2*s1*c2*c3*s4-2*c1*s3*s4-2*s1*s2*c4],
+	[0,
+	2*c2*c3*c4-2*s2*s4,
+	-2*s2*s3*c4-3*s3,
+	-2*s2*c3*s4+2*c2*c4
+	]])
+
+	return jacobian
+
+print(calculate_jacobian([0,0,0,0]))
